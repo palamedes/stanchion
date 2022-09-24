3808bebb048977439d060cd5e18134778483ce7f
palamedes <jason.ellis@yahoo.com>
2022-09-24 18:17:28 -0400

    adding annotation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,3 +6,16 @@ class User < ApplicationRecord
 
 
 end
+# --- Model: 'User' Annotation
+# Table Name: users
+#
+#  id                       :integer(8)     Primary Key, not null
+#
+#  email                    :string         not null
+#  encrypted_password       :string         not null
+#  remember_created_at      :datetime       
+#  reset_password_sent_at   :datetime       
+#  reset_password_token     :string         
+#
+#  created_at               :datetime       not null
+#  updated_at               :datetime       not null
