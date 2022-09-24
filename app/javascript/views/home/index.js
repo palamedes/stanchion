5465241f161f4c03a6d6f1058a195ad186afaf68
palamedes <jason.ellis@yahoo.com>
2022-09-24 16:59:38 -0400

    testing

---
+++ app/javascript/views/home/index.js
@@ -0,0 +1,10 @@
+$(function() {
+  // $('body').toast({
+  //   position: 'bottom attached',
+  //   title: 'Cookies!',
+  //   message: 'We use cookies on our website to give you the most relevant experience by remembering your preferences and repeat visits. <br /> By clicking “Accept All”, you consent to the use of ALL the cookies. However, you may visit "Cookie Settings" to provide a controlled consent.',
+  //   newestOnTop: true,
+  //   displayTime: 0,
+  // });
+});
+
