cead48934af2a58f2169630e25b34f3158030961
palamedes <jason.ellis@yahoo.com>
2022-10-03 20:21:37 -0400

    Updating devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for  :users,
+              :path => '',
+              :path_names => {  :sign_in =>       'login',
+                                :sign_out =>      'logout',
+                                :sign_up =>       '',
+                                :registration =>  'register',
+                                :edit =>          'edit',
+                                :cancel =>        'cancel',
+                                :confirmation =>  'verification'  }
+
 
   post :foo, to: "home#bar", path: '/baz/:id/floop'
   get :test, to: "home#test"
